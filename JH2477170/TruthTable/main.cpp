69be78791de7cfd51c6f34f503f8892add699438
Izanagi22 <jahsystem22@yahoo.com>
2014-03-12 14:04:39 -0700

    Signed-off-by: Izanagi22 <jahsystem22@yahoo.com>

---
+++ JH2477170/TruthTable/main.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File:   main.cpp
+ * Author: Jose A. Hernandez
+ *
+ * Created on March 12, 2014, 10:13 AM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Starts Here!!!
+int main(int argc, char** argv) {
+    //Declare Variables
+    bool x,y;
+    //Output Heading
+    //Output First Row
+    x=true;
+    y=true;
+    //Output Second Row
+    //Output Third Row
+    //Output Last Row
+    //Exit Stage Right!!!
+    return 0;
+}
+
+
