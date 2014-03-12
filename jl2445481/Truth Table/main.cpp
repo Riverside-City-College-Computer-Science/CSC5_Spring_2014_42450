07954299b0d3f8b101b81edb59924cddc522f905
lunaj388 <lunaj388@gmail.com>
2014-03-12 10:24:05 -0700

    Signed-off-by: lunaj388 <lunaj388@gmail.com>

---
+++ jl2445481/Truth Table/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author:Jose Luna
+ * Created on March 12, 2014, 10:13 AM
+ */
+
+//System Libraries
+#include <iostream>
+
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins here
+
+int main(int argc, char** argv) {
+    //Declare Variables
+    bool x,y;
+    x=true;
+    y=true;
+
+    return 0;
+}
+
