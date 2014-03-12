ba65ac7523896c0f151fe9eb3da94cc73c27f5ce
MDaw <MarwahDaw@gmail.com>
2014-03-12 10:32:54 -0700

    Signed-off-by: MDaw <MarwahDaw@gmail.com>

---
+++ md2499580/Truth Table/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: karen
+ *
+ * Created on March 12, 2014, 10:16 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
