2b57b415611101c53e4a3e71e57ed4d83d14ae6b
Zeke62 <zludwin63@gmail.com>
2014-03-12 10:44:34 -0700

    Signed-off-by: Zeke62 <zludwin63@gmail.com>

---
+++ zl031214/TruthTable/main.cpp
@@ -0,0 +1,29 @@
+/* 
+ * File:   main.cpp
+ * Author: Zachery Ludwin
+ * Created on March 12, 2014, 10:14 AM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables
+    bool x,y;
+    //Output Heading 
+    //Output First Row
+    x=true;
+    y=true;
+    //Output Second Row
+    //Output Third Row
+    //Output Last Row
+    
+    return 0;
+}
+
