533e11958b8c5c9bd7cf3e7d8d80839c239c3a5d
cesar2661 <obeso@juno.com>
2014-03-14 10:55:37 -0700

    Signed-off-by: cesar2661 <obeso@juno.com>

--- co2500608/nbproject/Makefile-variables.mk
+++ co2500608/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
-CND_ARTIFACT_NAME_Debug=truthtable
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/truthtable
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
-CND_PACKAGE_NAME_Debug=truthtable.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/truthtable.tar
+CND_PLATFORM_Debug=Cygwin_4.x_1-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x_1-Windows
+CND_ARTIFACT_NAME_Debug=co2500608
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x_1-Windows/co2500608
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x_1-Windows/package
+CND_PACKAGE_NAME_Debug=co2500608.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x_1-Windows/package/co2500608.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
-CND_ARTIFACT_NAME_Release=truthtable
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/truthtable
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
-CND_PACKAGE_NAME_Release=truthtable.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/truthtable.tar
+CND_PLATFORM_Release=Cygwin_4.x_1-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x_1-Windows
+CND_ARTIFACT_NAME_Release=co2500608
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x_1-Windows/co2500608
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x_1-Windows/package
+CND_PACKAGE_NAME_Release=co2500608.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x_1-Windows/package/co2500608.tar
 #
 # include compiler specific variables
 #
--- co2500608/nbproject/Package-Release.bash
+++ co2500608/nbproject/Package-Release.bash
@@ -6,15 +6,16 @@
 
 # Macros
 TOP=`pwd`
-CND_PLATFORM=Cygwin_4.x-Windows
+CND_PLATFORM=Cygwin_4.x_1-Windows
 CND_CONF=Release
 CND_DISTDIR=dist
 CND_BUILDDIR=build
+CND_DLIB_EXT=dll
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/truthtable
-OUTPUT_BASENAME=truthtable
-PACKAGE_TOP_DIR=truthtable/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/co2500608
+OUTPUT_BASENAME=co2500608
+PACKAGE_TOP_DIR=co2500608/
 
 # Functions
 function checkReturnCode
@@ -59,15 +60,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/truthtable/bin"
+makeDirectory "${NBTMPDIR}/co2500608/bin"
 copyFileToTmpDir "${OUTPUT_PATH}.exe" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}.exe" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/truthtable.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/co2500608.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/truthtable.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/co2500608.tar *
 checkReturnCode
 
 # Cleanup
