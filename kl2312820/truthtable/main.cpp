d16175091b5809b6994d6f80caa4332d358cd26d
klozano47 <klozano4@student.rcc.edu>
2014-03-17 10:13:32 -0700

    Signed-off-by: klozano47 <klozano4@student.rcc.edu>

--- kl2312820/truthtable/main.cpp
+++ kl2312820/truthtable/main.cpp
@@ -15,33 +15,33 @@ using namespace std;
 
 //Eecution Begins Here!
 int main(int argc, char** argv) {
-    //Declare Variables
+   //Declare Variables
     bool x=true;
     bool y=true;
-    //Output the headings
+   //Output the headings
     cout<<"X Y !X !Y X||Y X&&Y ";
     cout<<"X^Y X^Y^X X^Y^Y !(X&&Y) ";
     cout<<"!X||Y !(X||Y) !X&&!Y"<<endl;
-    //Output the first row of the truth table
+   //Output the first row of the truth table
     cout<<(x?'T':'F')<<" ";
     cout<<(y?'T':'F')<<" ";
     cout<<(!x?'T':'F')<<"  ";
     cout<<(!y?'T':'F')<<"    ";
     cout<<(x||y?'T':'F')<<"   ";
     cout<<(x&&y?'T':'F')<<"    ";
-    //Output he second row of the truth table
+   //Output he second row of the truth table
     y=false;
     cout<<(x^y?'T':'F')<<"    ";
     cout<<(x^y^x?'T':'F')<<"     ";
     cout<<(x^y^y?'T':'F')<<"     ";
     cout<<(!(x&&y)?'T':'F')<<"      ";
-    //Output the third row of the truth table
+   //Output the third row of the truth table
     y=true;
     x=false;
     cout<<(!x||y?'T':'F')<<"       ";
     cout<<(!(x||y)?'T':'F')<<"      ";
     cout<<(!x&&!y?'T':'F')<<" ";
-
+   //Exit Stage Right!
     return 0;
 }
 
