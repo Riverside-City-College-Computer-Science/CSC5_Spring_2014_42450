87558dc5651947d64f09ea399734bfd955dbda3d
victormedel <victor.medel@roadrunner.com>
2014-03-19 10:10:04 -0700

    Signed-off-by: victormedel <victor.medel@roadrunner.com>

--- vm2519364/Assignment 3/switchExample/main.cpp
+++ vm2519364/Assignment 3/switchExample/main.cpp
@@ -22,11 +22,24 @@ int main(int argc, char** argv) {
     char score=rand()%51+50;//[50,100]
     char grade;
     //What score corresponds with the correct grade
-    (score>89)?grade='A':
-    (score>79)?grade='B':
-    (score>69)?grade='C':
-    (score>59)?grade='D':
-               grade='F';
+    switch(score>89){
+        case true: grade='A';break;
+        default:
+           switch(score>79){
+               case true: grade='B';break;
+               case false:
+                   switch(score>69){
+               case true: grade='C';break;
+               default:
+                          switch (score>59){
+                             case true: grade='D';break;
+                             default: grade='F';
+                       
+                          }
+                   }
+           }
+    }
+
     //Output the results
     cout<<"A score of "<<static_cast<int>(score);
     cout<<" gives a grade = "<<grade<<endl;   
