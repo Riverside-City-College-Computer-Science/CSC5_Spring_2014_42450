d8ff6c78b6f9c61a1cb04abba0586d4e5490f888
Izanagi22 <jahsystem22@yahoo.com>
2014-03-20 00:07:53 -0700

    Signed-off-by: Izanagi22 <jahsystem22@yahoo.com>

--- JH2477170/TruthTablev2/nbproject/Makefile-variables.mk
+++ JH2477170/TruthTablev2/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
-CND_ARTIFACT_NAME_Debug=truthtable
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/truthtable
+CND_ARTIFACT_NAME_Debug=truthtablev2
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/truthtablev2
 CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
-CND_PACKAGE_NAME_Debug=truthtable.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/truthtable.tar
+CND_PACKAGE_NAME_Debug=truthtablev2.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/truthtablev2.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
-CND_ARTIFACT_NAME_Release=truthtable
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/truthtable
+CND_ARTIFACT_NAME_Release=truthtablev2
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/truthtablev2
 CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
-CND_PACKAGE_NAME_Release=truthtable.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/truthtable.tar
+CND_PACKAGE_NAME_Release=truthtablev2.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/truthtablev2.tar
 #
 # include compiler specific variables
 #
