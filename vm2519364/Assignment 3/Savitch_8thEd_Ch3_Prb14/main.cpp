b033faad705cc01ab893eb14c3754e79a8af17e6
victormedel <victor.medel@roadrunner.com>
2014-03-23 09:39:28 -0700

    Signed-off-by: victormedel <victor.medel@roadrunner.com>

---
+++ vm2519364/Assignment 3/Savitch_8thEd_Ch3_Prb14/main.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   main.cpp
+ * Author: Victor Medel
+ * Created on March 22, 2014, 10:11 PM
+ * Purpose: Find Prime Number between 3 and 100
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    int n;
+    
+    
+    if ((n==1)||(n==n))
+        
+    
+    //Exit Stage Right
+    return 0;
+}
+
