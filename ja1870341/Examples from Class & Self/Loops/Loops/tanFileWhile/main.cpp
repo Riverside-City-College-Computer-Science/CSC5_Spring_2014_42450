aca145dc4e1f52e61569e0e880734eec78f315d4
Bluebarron <adams.jon.c@gmail.com>
2014-04-11 18:54:29 -0700

    Adding class examples from the week

---
+++ ja1870341/Examples from Class & Self/Loops/Loops/tanFileWhile/main.cpp
@@ -0,0 +1,47 @@
+/* 
+ * File:   main.cpp
+ * Author: Adams, Jonathan
+ * Created on April 2, 2014, 9:40 AM
+ */
+//System Libraries
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+#include <cstring>
+using namespace std;
+//Global Constants
+//Function Prototypes
+//Executioner
+int main(int argc, char** argv) {
+//Declare variables
+    ifstream input;
+    ofstream output;
+    //Declare file name
+    string inFile="inDeg.dat";
+    char outFile[]="trigTab.dat";
+    //Read the degree data from a file
+    int degrees;
+    input.open(inFile.c_str());
+    output.open(outFile);
+    while (input>>degrees) {
+        float angRad=degrees*atan(1)/45;
+        output<<fixed<<setprecision(4)<<showpoint;
+        output<<setw(10)<<degrees;
+        output<<setw(10)<<angRad;
+        output<<setw(10)<<sin(angRad);
+        output<<setw(10)<<cos(angRad);
+        if (tan(angRad)>100)
+            output<<setw(10)<<100<<endl;
+        else if (tan(angRad)<-100)
+            output<<setw(10)<<-100<<endl;
+        else
+            output<<setw(10)<<tan(angRad)<<endl;
+    }
+    //Close the files
+    input.close();
+    output.close();
+    //Exit Stage Right
+    return 0;
+}
+
