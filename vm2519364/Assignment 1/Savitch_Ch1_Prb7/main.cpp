8673c735863ca4a8ff0f76134b48c5a5acb6ad17
victormedel <victor.medel@roadrunner.com>
2014-04-23 09:34:45 -0700

    Signed-off-by: victormedel <victor.medel@roadrunner.com>

---
+++ vm2519364/Assignment 1/Savitch_Ch1_Prb7/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: Victor Medel
+ * Created on February 26, 2014, 5:24 PM
+ * Purpose: Create a large message that reads: C S !
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Starts Here!
+int main(int argc, char** argv) {
+//Output C S !
+    cout<<"**********************************************\n";
+    cout<<"        C C C                 S S S S       !!\n";
+    cout<<"      C       C             S        S      !!\n";
+    cout<<"     C                     S                !!\n";
+    cout<<"     C                      S               !!\n";
+    cout<<"     C                       S S S S        !!\n";
+    cout<<"     C                               S      !!\n";
+    cout<<"     C                                S     !!\n";
+    cout<<"      C       C              S       S        \n";
+    cout<<"        C C C                 S S S S       00\n";
+    cout<<"**********************************************\n";
+    cout<<"\n";
+    cout<<"\n";
+    cout<<"       Computer Science is Cool Stuff!!!      \n";
+    
+    
+//Exit Stage Right    
+    return 0;
+
+}
+
