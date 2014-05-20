7d8985621f525f4871bb66e10de3aaa3bed89c23
victormedel <victor.medel@roadrunner.com>
2014-05-19 22:30:17 -0700

    Signed-off-by: victormedel <victor.medel@roadrunner.com>

--- vm2519364/Assignment 6/Savitch_8thEd_Ch6_Prb6/main.cpp
+++ vm2519364/Assignment 6/Savitch_8thEd_Ch6_Prb6/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdlib>
 using namespace std;
 
 //Global Constants
@@ -19,31 +20,27 @@ void rmvBlnk(ifstream& , ofstream&);
 //Execution Starts Here
 int main(int argc, char** argv) {
     //Declare Variables
-    
     ifstream infile;
     ofstream outfile;
-    
     infile.open("input.dat");
+     if(infile.fail()){
+        cout<<"Input file failed to open.\n";
+        exit(1);
+    }
     outfile.open("output.dat");
-    
+     if(outfile.fail()){
+        cout<<"Output file failed to open.\n";
+        exit(1);
+    }
     rmvBlnk(infile, outfile);
-            
     infile.close();
-    outfile.close();
-            
+    outfile.close();    
     //Exit Stage Right
     return 0;
 }
-
 void rmvBlnk(ifstream& infile, ofstream& outfile){
     string next;
-    infile.get(next);
-    while(!infile.eof()){
-        if(next=='  ')
-            outfile<<" ";
-        else
-            outfile<<next;
-        infile.get(next);
-    }
-    
+    while (infile>>next){
+        outfile<<next<<" ";
+    } 
 }
---
+++ vm2519364/Assignment 6/Savitch_8thEd_Ch6_Prb7/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: Victor Medel
+ * Created on May 19, 2014, 10:28 PM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Exit Stage Right
+    return 0;
+}
+
